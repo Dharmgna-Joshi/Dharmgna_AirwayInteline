861c909f2c475b0c9bbe2979d31f3a3fff953ae8
dharmgnajoshi131199@gmail.com <dharmgnajoshi131199@gmail.com>
2023-12-19 21:40:28 -0500

    Camera Openning app

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [photoUri, setPhotoUri] = useState(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+      await MediaLibrary.requestPermissionsAsync();
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      try {
+        const photo = await cameraRef.current.takePictureAsync();
+        const asset = await MediaLibrary.createAssetAsync(photo.uri);
+        setPhotoUri(asset.uri);
+        console.log('Photo saved to gallery:', asset.uri);
+      } catch (error) {
+        console.error('Error taking picture:', error);
+      }
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {photoUri ? (
+        <View style={styles.previewContainer}>
+          <Image source={{ uri: photoUri }} style={styles.preview} />
+        </View>
+      ) : (
+        <Camera style={styles.camera} ref={cameraRef}>
+          <View style={styles.buttonContainer}>
+            <Button title="Take Photo" onPress={takePicture} />
+          </View>
+        </Camera>
+      )}
     </View>
   );
 }
@@ -13,8 +56,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  previewContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  preview: {
+    width: 300,
+    height: 400,
+    marginBottom: 20,
   },
 });
